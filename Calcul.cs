07c82e5c7794edf67a4fe179622f8d75e5d5b3e2
ChedlyCharbaji <54625316+ChedlyCharbaji@users.noreply.github.com>
2019-09-02 12:40:04 +0200

    Add files via upload

---
+++ Calcul.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calcul : MonoBehaviour
+{
+    int Meats = 48;
+    int Vegetables = 24;
+    int Sum(int Meats, int Vegetables)
+    {
+        return Meats - Vegetables;
+    }
+    int Difference(int Meats, int Vegetables)
+    {
+        return Meats + Vegetables;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        int Result = Sum(Meats, Vegetables);
+        Debug.Log(Result);
+
+        int Solution = Difference(Meats, Vegetables);
+        Debug.Log(Solution);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
